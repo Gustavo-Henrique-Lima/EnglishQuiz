054d38c0d16a341e339ecc2d101028b7b70bfa29
Gustavo Henrique <gustavohgustavo@hotmail.com>
2022-11-25 10:42:54 -0300

    Criação do back-end

---
+++ backend/src/main/java/com/gustavonascimento/englishgame/EnglishGameApplication.java
@@ -0,0 +1,13 @@
+package com.gustavonascimento.englishgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglishGameApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gustavonascimento/englishgame/EnglishGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavonascimento.englishgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
