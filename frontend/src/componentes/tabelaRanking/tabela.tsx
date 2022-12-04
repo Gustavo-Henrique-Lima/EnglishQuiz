628aa4dddd8f967c87bd629facb95f366bc8c75d
Gustavo Henrique <gustavohgustavo@hotmail.com>
2022-12-04 12:13:01 -0300

    Criação da tabela de ranking ainda sem filtrar por nível

---
+++ frontend/src/componentes/tabelaRanking/tabela.tsx
@@ -0,0 +1,40 @@
+import './styles.css'
+
+function TabelaRanking()
+{
+    return(
+        <div className="englishproject-card">
+                <h2 className="englishproject-ranking-title">Ranking</h2>
+                <div>
+                <table className="englishproject-ranking-table">
+          <thead>
+            <tr>
+              <th>Posição</th>
+              <th>Nickname</th>
+              <th>Pontuação</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>#341</td>
+              <td>Anakin</td>
+              <td>9</td>
+            </tr>
+            <tr>
+              <td>#341</td>
+              <td>Anakin</td>
+              <td>8</td>
+            </tr>
+            <tr>
+              <td>#341</td>
+              <td>Anakin</td>
+              <td>7</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    )
+}
+
+export default TabelaRanking
